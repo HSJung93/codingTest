ab2dac02bd9cb0aa63956176f506bee80d9d0dc3
Hoesung Jung <jrps2212@gmail.com>
2021-09-07 13:11:35 +0900

    bt

--- 14_BackTracking.py
+++ 14_BackTracking.py
@@ -18,4 +18,51 @@ def solve():
     solutions = []
     state = set()
     search(state, solutions)
-    return solution
+    return solutions
+
+# N-Queen
+import sys
+n = int(sys.stdin.readline())
+    
+def solveNQueens():
+    solutions = []
+    state = []
+    search(state, solutions, n)
+    print(solutions)
+
+def is_valid_state(state, n):
+    return len(state) == n
+
+def get_candidates(state, n):
+    if not state:
+        return range(n)
+    
+    position = len(state)
+    candidates = set(range(n))
+            
+    for row, col in enumerate(state):
+        candidates.discard(col)
+        dist = position - row
+        candidates.discard(col + dist)
+        candidates.discard(col - dist)
+    return candidates
+
+def search(state, solutions, n):
+    if is_valid_state(state, n):
+        state_string = state_to_string(state, n)
+        solutions.append(state_string)
+        return
+    
+    for candidate in get_candidates(state, n):
+        state.append(candidate)
+        search(state, solutions, n)
+        state.pop()
+        
+def state_to_string(state, n):
+    ret = []
+    for i in state: 
+        string = '.' * i + 'Q' + '.' * (n-i-1)
+        ret.append(string)
+    return ret
+
+solveNQueens()
