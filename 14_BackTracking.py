6b1f998bb4e6ec4830b883023b39d5396d40b332
Hoesung Jung <jrps2212@gmail.com>
2021-09-08 02:29:50 +0900

    bt

--- 14_BackTracking.py
+++ 14_BackTracking.py
@@ -24,7 +24,8 @@ def solve():
 
 # N-Queen
 import sys
-n = int(sys.stdin.readline())
+n = 4
+# n = int(sys.stdin.readline())
     
 def solveNQueens():
     solutions = []
@@ -68,3 +69,130 @@ def state_to_string(state, n):
     return ret
 
 solveNQueens()
+
+## solveSudoku (boj2580)
+# input
+# 0 3 5 4 6 9 2 7 8
+# 7 8 2 1 0 5 6 0 9
+# 0 6 0 2 7 8 1 3 5
+# 3 2 1 0 4 6 8 9 7
+# 8 0 4 9 1 3 5 0 6
+# 5 9 6 8 2 0 4 1 3
+# 9 1 7 6 5 2 0 8 0
+# 6 0 3 7 0 1 9 5 2
+# 2 5 8 3 9 4 7 6 0
+# output
+# 1 3 5 4 6 9 2 7 8
+# 7 8 2 1 3 5 6 4 9
+# 4 6 9 2 7 8 1 3 5
+# 3 2 1 5 4 6 8 9 7
+# 8 7 4 9 1 3 5 2 6
+# 5 9 6 8 2 7 4 1 3
+# 9 1 7 6 5 2 3 8 4
+# 6 4 3 7 8 1 9 5 2
+# 2 5 8 3 9 4 7 6 1
+
+# when traversing the grid
+from itertools import product
+import sys
+
+SHAPE = 9
+GRID = 3
+EMPTY = '0'
+DIGITS = set([str(num) for num in range(1, SHAPE+1)])
+board = []
+
+for _ in range(SHAPE):
+    board.append(sys.stdin.readline().split())
+
+def solveSudoku():
+    search(board)
+
+def is_valid_state(board):
+    for row in get_rows(board):
+        if not set(row) == DIGITS:
+            return False
+        
+    for col in get_cols(board):
+        if not set(col) == DIGITS:
+            return False
+        
+    for grid in get_grids(board):
+        if not set(grid) == DIGITS:
+            return False
+        
+    return True
+        
+def get_candidates(board, row, col):
+    used_digits = set()
+    used_digits.update(get_kth_row(board, row))
+    used_digits.update(get_kth_col(board, col))
+    used_digits.update(get_grid_at_row_col(board, row, col))
+    used_digits -= set([EMPTY])
+    candidates= DIGITS - used_digits
+    return candidates
+
+def search(board):
+    if is_valid_state(board):
+        return True
+    
+    for row_idx, row in enumerate(board):
+        for col_idx, elm in enumerate(row):
+            if elm == EMPTY:
+                for candidate in get_candidates(board, row_idx, col_idx):
+                    board[row_idx][col_idx] = candidate
+                    
+                    #recurse on the modified board
+                    is_solved = search(board)
+                    if is_solved:
+                        return True
+                    else:
+                        #undo the wrong guess and start anew
+                        board[row_idx][col_idx] == EMPTY
+                        
+                # exhausted all candidates but none solves the problem
+                return False
+    return True
+
+# helper functions for retrieving rows, cols, and grids
+def get_kth_row(board, k):
+    return board[k]
+
+def get_rows(board):
+    for i in range(SHAPE):
+        yield board[i]
+
+def get_kth_col(board, k):
+    return [
+        board[row][k] for row in range(SHAPE)
+    ]
+
+def get_cols(board):
+    for col in range(SHAPE):
+        ret = [
+                board[row][col] for row in range(SHAPE)
+        ]
+        yield ret
+
+def get_grid_at_row_col(board, row, col):
+    row = row // GRID * GRID
+    col = col // GRID * GRID
+    return [
+        board[r][c] for r, c in 
+        product(range(row, row + GRID), range(col, col + GRID))
+    ]
+
+def get_grids(board):
+    for row in range(0, SHAPE, GRID):
+        for col in range(0, SHAPE, GRID):
+            grid = [
+                board[r][c] for r, c in 
+                product(range(row, row + GRID), range(col, col + GRID))
+            ]
+            yield grid
+            
+solveSudoku()
+
+for i in range(SHAPE):
+    line =' '.join(board[i])
+    print(line)
